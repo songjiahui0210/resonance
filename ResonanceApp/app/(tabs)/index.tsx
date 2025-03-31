c0b91e20b9aa59778c0497e688c467c824cde84d
JielingGong <serenagong01@outlook.com>
2025-03-31 16:46:08 -0700

    Added some basic CI workflows

--- ResonanceApp/app/(tabs)/index.tsx
+++ ResonanceApp/app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, View, Text, TextInput, Button, ScrollView, TouchableOpacity
 import Slider from '@react-native-community/slider';
 import styles from '../appStyles';
 
-const GEMINI_API_KEY = 'AIzaSyBPbgUjqjpO-cWV61Zs6xeypo-dSz2sCnY';
+const GEMINI_API_KEY = 'AIzaSyBb0WL25dnSdt25brIgqVbmBgb1BF7Rv58';
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
 
 function App() {
